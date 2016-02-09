d9dec2c6e7aa3180e795ebaa379a041182bca3f8
slembcke <slembcke@gmail.com>
2016-02-08 22:34:39 -0600

    Better example

--- example.lua
+++ example.lua
@@ -2,13 +2,13 @@ local template = require("elua")
 --local dbg = require("debugger")
 --dbg.call(function()
 
-local file = io.open("example.elua", "r")
-print(template.compile(file:read("a")))
+--local file = io.open("example.elua", "r")
+--print(template.compile(file:read("a")))
 
 local chunk, err = template.loadFile("example.elua")
 if err then error(err) end
 
-local output = chunk{foo = "bar"}
+local output = chunk{count = 99}
 print(output)
 
 --end)
