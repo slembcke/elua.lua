030a27ec8623ef9c71cdc627120835da0584ee5d
slembcke <slembcke@gmail.com>
2024-06-06 14:53:01 -0500

    restructure

--- elua.lua
+++ elua.lua
@@ -1,62 +1,62 @@
-local template = {}
+local append, join, format = table.insert, table.concat, string.format
 
-local BEGIN = "[====["
-local END = "]====]"
+-- return a list and a function that appends its arguments into the list
+local function appender(lines)
+	return lines, function(...) append(lines, join{...}) end
+end
 
-local EQUALS = string.byte("=")
-local sub = string.sub
+local elua = {}
 
-function template.compile(source)
-	local fragments = {"local __OUT, _ENV = ...;"}
-	
-	local cursor = 1
-	function push(...) for _, str in ipairs{...} do table.insert(fragments, str) end end
+function elua.generate(template)
+	-- push an initial line to recieve args from the wrapping closure
+	local cursor, fragments, append = 1, appender{"local __elua, _ENV = ...\n"}
 	
 	while true do
-		str, code = string.match(source, "(.-)({{.-}})", cursor)
-		if str == nil and code == nil then
-			-- Push the remainder of the string.
-			push("__OUT"..BEGIN, sub(source, cursor, -1), END.."; ")
-			break
-		else
-			-- Push the string component.
-			push("__OUT"..BEGIN, str, END.."; ")
-			
-			if string.byte(code, 3) == EQUALS then
-				-- Push an evaluation expression {{=...}}
-				push("__OUT(", sub(code, 4, -3), "); ")
-			else
-				-- Push a chunk {{...}}
-				push(sub(code, 3, -3), "; ")
-			end
+		-- find a code block
+		local i0, i1, m1, m2 = template:find("{{(=?)(.-)}}", cursor)
+		
+		if i0 == nil then
+			-- if no code block, append remainder and join
+			append("__elua", format("%q", template:sub(cursor, #template)))
+			return join(fragments, "; ")
+		elseif cursor ~= i0 then
+			-- if there is text to output, output it
+			append("__elua", format("%q", template:sub(cursor, i0 - 1)))
 		end
 		
-		cursor = cursor + #str + #code
+		if m1 == "=" then
+			-- append expression
+			append("__elua(", m2, ")")
+		else
+			-- append code
+			append(m2)
+		end
+		
+		cursor = i1 + 1
 	end
-	
-	return table.concat(fragments)
 end
 
-function template.load(str, name)
-	local compiled = template.compile(str)
-	local chunk, err = load(compiled, name or "COMPILED TEMPLATE", "t", nil)
+function elua.compile(template, name)
+	local template_code = elua.generate(template)
+	
+	-- compile the template's lua code
+	local chunk, err = load(template_code, name or "COMPILED TEMPLATE", "t")
 	if err then return nil, err end
 	
+	-- wrap chunk in closure to collect and join it's output fragments
 	return function(env)
-		local results = {}
-		local function out(value) table.insert(results, tostring(value)) end
-		
-		chunk(out, env or nil)
-		return table.concat(results)
+		local fragments, append = appender{}
+		chunk(append, env)
+		return join(fragments)
 	end
 end
 
-function template.loadFile(filename)
+function elua.compile_file(filename)
 	local file = io.open(filename, "r")
 	local source = file:read("a")
 	file:close()
 	
-	return template.load(source, filename)
+	return elua.compile(source, filename)
 end
 
-return template
+return elua
